66ff9b5fb651140a90e593bbcfa5ccfcbc976915
Dawson Schmitt <dischmitt@usf.edu>
2024-05-17 15:31:43 -0400

    Add output functionality to display bill, tip, and total.

--- cc_1.js
+++ cc_1.js
@@ -1,6 +1,12 @@
-function tippercentage(bill) {
-    return bill > 300 || bill < 50 ? '15%' : '20%'
+function calcTipPercentage(bill) {
+    return bill > 300 || bill < 50 ? .15 : .2
 }
-var tip
-tip = tippercentage(300)
-console.log(tip)
+function calcTipAmount(bill , tippercentage) {
+    return bill * tippercentage
+}
+var bill = 200
+var tip = calcTipAmount(bill , calcTipPercentage(bill))
+var total = bill + tip
+console.log('The bill was: $'+bill) 
+console.log('The tip was: $'+tip)
+console.log('And the total value: $' +total) 
