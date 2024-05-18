ebcbb7551aead8acc34abba7c62d2ab928c95737
Dawson Schmitt <dischmitt@usf.edu>
2024-05-17 22:09:35 -0400

    Create and test calcTip function.

--- cc_1.js
+++ cc_1.js
@@ -1,12 +1,12 @@
 function calcTipPercentage(bill) {
     return bill > 300 || bill < 50 ? .15 : .2
 }
-function calcTipAmount(bill , tippercentage) {
-    return bill * tippercentage
+function calcTip(bill) {
+    return calcTipPercentage(bill) * bill
 }
-var bill = 200
-var tip = calcTipAmount(bill , calcTipPercentage(bill))
-var total = bill + tip
+var bill = 100
+var calculateTip = calcTip(bill)
+var total = bill + calcTip(bill)
 console.log('The bill was: $'+bill) 
-console.log('The tip was: $'+tip)
+console.log('The tip was: $'+calcTip(bill))
 console.log('And the total value: $' +total) 
